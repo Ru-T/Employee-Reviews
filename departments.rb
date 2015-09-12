4ed595a6146881edd313cd0bdc88aec834c7947a
Ru-T <rwajnberg@gmail.com>
2015-09-12 17:54:26 -0400

    Refactor of solution to raise.

--- departments.rb
+++ departments.rb
@@ -15,8 +15,16 @@ attr_reader :name, :employees
     @employees.reduce(0){|sum, employee| sum + employee.salary}
   end
 
+  # def department_raise(amount)
+  #   @employees.each do |employee|
+  #     if employee.performance == true
+  #       employee.give_raise(amount/@employees.length)
+  #     end
+  #   end
+  # end
+
   def department_raise(amount)
-    @employees.each do |employee|
+    @employees.select do |employee|
       if employee.performance == true
         employee.give_raise(amount/@employees.length)
       end
@@ -24,10 +32,20 @@ attr_reader :name, :employees
   end
 
   def department_raise_block(amount)
-    @employees.each do |employee|
+    @employees.select do |employee|
       if yield(employee)
         employee.give_raise(amount/@employees.length)
       end
     end
   end
+
+  # def department_raise_block(amount)
+  #   chosen_employees = []
+  #   chosen_employees << @employees.each { |employee| yield(employee)}
+  #   chosen_employees.each do |employee|
+  #       employee.give_raise(amount/chosen_employees.length)
+  #   end
+  # end
+
+
 end
--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'byebug'
 
 require './employee_reviews.rb'
 
