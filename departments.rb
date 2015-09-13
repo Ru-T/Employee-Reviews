1dc4d27c8ebf7b7dc3ed64dd9430df7f7163fdf3
Ru-T <rwajnberg@gmail.com>
2015-09-12 20:12:10 -0400

    Slimed reviews test with matching text. Next step: make this intelligent.

--- departments.rb
+++ departments.rb
@@ -15,54 +15,32 @@ attr_reader :name, :employees
     @employees.reduce(0){|sum, employee| sum + employee.salary}
   end
 
-  # def department_raise(amount)
-  #   @employees.each do |employee|
-  #     if employee.performance == true
-  #       employee.give_raise(amount/@employees.length)
-  #     end
-  #   end
-  # end
-
-    def department_raise(amount)
-      chosen_employees = []
-      @employees.each do |employee|
-        if employee.performance == true
-          chosen_employees << employee
-        end
-      end
-      @employees.each do |employee|
-        if employee.performance == true
-          employee.give_raise(amount/chosen_employees.length)
-        end
+  def department_raise(amount)
+    chosen_employees = []
+    @employees.each do |employee|
+      if employee.performance == true
+        chosen_employees << employee
       end
     end
-
-    def department_raise_block(amount)
-      chosen_employees = []
-      @employees.each do |employee|
-        if yield(employee)
-          chosen_employees << employee
-        end
-      end
-      @employees.each do |employee|
-        if yield(employee)
-          employee.give_raise(amount/chosen_employees.length)
-        end
+    @employees.each do |employee|
+      if employee.performance == true
+        employee.give_raise(amount/chosen_employees.length)
       end
     end
+  end
 
-
-  #
-  #
-  # def department_raise_block(amount)
-  #   @employees.select do |employee|
-  #     if yield(employee)
-  #       employee.give_raise(amount/@employees.length)
-  #     end
-  #   end
-  # end
-
-
-
+  def department_raise_block(amount)
+    chosen_employees = []
+    @employees.each do |employee|
+      if yield(employee)
+        chosen_employees << employee
+      end
+    end
+    @employees.each do |employee|
+      if yield(employee)
+        employee.give_raise(amount/chosen_employees.length)
+      end
+    end
+  end
 
 end
--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -68,18 +68,17 @@ class EmployeeReviewTest < Minitest::Test
     # employee = Employee.new("Ruti")
     # review = "This employee is truly awesome!"
     # assert_equal ["This employee is truly awesome!"], employee.reviews(review)
-    employee1 = Employee.new("Zeke")
-    review1 = "Zeke is a very positive person and encourages those around him, but he has not done well technically this year.  There are two areas in which Zeke has room for improvement.  First, when communicating verbally (and sometimes in writing), he has a tendency to use more words than are required.  This conversational style does put people at ease, which is valuable, but it often makes the meaning difficult to isolate, and can cause confusion.
-    Second, when discussing new requirements with project managers, less of the information is retained by Zeke long-term than is expected.  This has a few negative consequences: 1) time is spent developing features that are not useful and need to be re-run, 2) bugs are introduced in the code and not caught because the tests lack the same information, and 3) clients are told that certain features are complete when they are inadequate.  This communication limitation could be the fault of project management, but given that other developers appear to retain more information, this is worth discussing further."
-    employee1.reviews(review1)
-    assert_equal false, employee1.performance
 
-    employee2 = Employee.new("Xavier")
-    review2 = "Xavier is a huge asset to SciMed and is a pleasure to work with.  He quickly knocks out tasks assigned to him, implements code that rarely needs to be revisited, and is always willing to help others despite his heavy workload.  When Xavier leaves on vacation, everyone wishes he didn't have to go
-    Last year, the only concerns with Xavier performance were around ownership.  In the past twelve months, he has successfully taken full ownership of both Acme and Bricks, Inc.  Aside from some false starts with estimates on Acme, clients are happy with his work and responsiveness, which is everything that his managers could ask for."
+    employee2 = Employee.new("Zeke")
+    review2 = "Zeke is a very positive person and encourages those around him, but he has not done well technically this year.  There are two areas in which Zeke has room for improvement.  First, when communicating verbally (and sometimes in writing), he has a tendency to use more words than are required.  This conversational style does put people at ease, which is valuable, but it often makes the meaning difficult to isolate, and can cause confusion.
+    Second, when discussing new requirements with project managers, less of the information is retained by Zeke long-term than is expected.  This has a few negative consequences: 1) time is spent developing features that are not useful and need to be re-run, 2) bugs are introduced in the code and not caught because the tests lack the same information, and 3) clients are told that certain features are complete when they are inadequate.  This communication limitation could be the fault of project management, but given that other developers appear to retain more information, this is worth discussing further."
     employee2.reviews(review2)
-    puts employee2.performance
-    assert_equal true, employee2.performance
+    assert_equal false, employee2.performance
+
+    employee3 = Employee.new("Wanda")
+    review3 = "Wanda has been an incredibly consistent and effective developer.  Clients are always satisfied with her work, developers are impressed with her productivity, and she's more than willing to help others even when she has a substantial workload of her own.  She is a great asset to Awesome Company, and everyone enjoys working with her.  During the past year, she has largely been devoted to work with the Cement Company, and she is the perfect woman for the job.  We know that work on a single project can become monotonous, however, so over the next few months, we hope to spread some of the Cement Company work to others.  This will also allow Wanda to pair more with others and spread her effectiveness to other projects."
+    employee3.reviews(review3)
+    assert_equal true, employee3.performance
   end
 
   def test_employee_performance #Mark whether an employee is performing satisfactorily or not satisfactorily.
@@ -136,21 +135,5 @@ class EmployeeReviewTest < Minitest::Test
     assert_equal 98000, employee2.salary
     assert_equal 124500, employee3.salary
   end
-
-  # def test_set_performance_by_scanning_reviews
-  #   employee = Employee.new("Zeke")
-  #   review = "Zeke is a very positive person and encourages those around him, but he has not done well technically this year.  There are two areas in which Zeke has room for improvement.  First, when communicating verbally (and sometimes in writing), he has a tendency to use more words than are required.  This conversational style does put people at ease, which is valuable, but it often makes the meaning difficult to isolate, and can cause confusion.
-  #   Second, when discussing new requirements with project managers, less of the information is retained by Zeke long-term than is expected.  This has a few negative consequences: 1) time is spent developing features that are not useful and need to be re-run, 2) bugs are introduced in the code and not caught because the tests lack the same information, and 3) clients are told that certain features are complete when they are inadequate.  This communication limitation could be the fault of project management, but given that other developers appear to retain more information, this is worth discussing further."
-  #   employee.reviews(review)
-  #   employee.auto_set_performance(review)
-  #   assert_equal false, employee.performance
-  #
-  #   employee = Employee.new("Xavier")
-  #   review = "Xavier is a huge asset to SciMed and is a pleasure to work with.  He quickly knocks out tasks assigned to him, implements code that rarely needs to be revisited, and is always willing to help others despite his heavy workload.  When Xavier leaves on vacation, everyone wishes he didn't have to go
-  #   Last year, the only concerns with Xavier performance were around ownership.  In the past twelve months, he has successfully taken full ownership of both Acme and Bricks, Inc.  Aside from some false starts with estimates on Acme, clients are happy with his work and responsiveness, which is everything that his managers could ask for."
-  #   employee.reviews(review)
-  #   employee.auto_set_performance
-  #   assert_equal true, employee.performance
-  # end
 
 end
